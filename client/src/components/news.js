36649c13531ee2fa702722b08048885eabe1b65a
MoniqueOg <moniqueogunk@icloud.com>
2023-03-14 01:18:23 +0000

    feat: wip

--- client/src/components/news.js
+++ client/src/components/news.js
@@ -5,31 +5,26 @@ const cheerio = require("cheerio");
 const url = 'https://uk.finance.yahoo.com/topic/news/';
 
 fetch(url)
-  .then(response => response.text())
-  .then(data => {
-    const $ = cheerio.load(data);
+    .then(response => response.text())
+    .then(data => {
+        const $ = cheerio.load(data);
+        const title = $('h3.Mb(5px)').text().trim();
+        const author = $('div.Yahoo-Finance').text().trim();
+        const date = $('div.article-date').text().trim();
+        let content = '';
+        $('div.article-content p').each((i, el) => {
+            content += $(el).text().trim() + '\n';
+        });
 
-    const title = $('h1.article-title').text().trim();
-
-    const author = $('div.article-author').text().trim();
-
-    const date = $('div.article-date').text().trim();
-
-    let content = '';
-    $('div.article-content p').each((i, el) => {
-      content += $(el).text().trim() + '\n';
+        const article = {
+            title: title,
+            author: author,
+            date: date,
+            content: content
+        };
+        console.log(article);
+    })
+    .catch(error => {
+        console.error('Error fetching news article:', error);
     });
-
-    const article = {
-        title: title,
-        author: author,
-        date: date,
-        content: content
-    };
-
-    console.log(article);
-  })
-  .catch(error => {
-    console.error('Error fetching news article:', error);
-  });
 
