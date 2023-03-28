8f59cd8b06210ba59cf94b409b5c81155d629d19
MoniqueOg <moniqueogunk@icloud.com>
2023-03-28 21:43:55 +0100

    about us inline styling applied

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,25 +3,25 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className='about'>
-            <h1>About</h1>
-            Hi there! Are you looking to take your trading game to the next level? Well, you've come to the right place! 
-            Our trading aid is the perfect tool for anyone looking to make informed decisions in the stock market.
-<br/>
-            With our glossary, you'll never feel lost or confused by industry jargon again. 
-            And our informative articles cover everything from market trends to trading strategies,
-             written by experienced traders and market experts.
-<br/>
-            But that's not all – our live stock chart feature provides up-to-date information on stock prices, 
-            so you can make informed trading decisions.
-            And for novice traders, our paper trading simulator is an ideal tool to learn and gain experience without risking any actual money.
-            <br/>
-            Plus, our powerful search engine makes it easy to find the stock results you're looking for. 
-            Just search by stock symbol, company name, or any other criteria,
-            and you'll have instant access to relevant information.
-            <br/>
-            So, what are you waiting for? Give our trading aid a try and see for yourself how it can help take your trading to the next level. 
-            We're confident you'll love it!
+        <div style={{  padding: '20px' }}>
+            <h1 style={{ color: '#333', fontSize: '2rem', marginBottom: '1rem' }}>About</h1>
+            <p style={{ color: '#333', fontSize: '1.2rem', lineHeight: '1.5' }}>Hi there! Are you looking to take your trading game to the next level? Well, you've come to the right place!
+                Our trading aid is the perfect tool for anyone looking to make informed decisions in the stock market.</p>
+            <br />
+            <p style={{ color: '#333', fontSize: '1.2rem', lineHeight: '1.5' }}>With our glossary, you'll never feel lost or confused by industry jargon again.
+                And our informative articles cover everything from market trends to trading strategies,
+                written by experienced traders and market experts.</p>
+            <br />
+            <p style={{ color: '#333', fontSize: '1.2rem', lineHeight: '1.5' }}>But that's not all – our live stock chart feature provides up-to-date information on stock prices,
+                so you can make informed trading decisions.
+                And for novice traders, our paper trading simulator is an ideal tool to learn and gain experience without risking any actual money.</p>
+            <br />
+            <p style={{ color: '#333', fontSize: '1.2rem', lineHeight: '1.5' }}>Plus, our powerful search engine makes it easy to find the stock results you're looking for.
+                Just search by stock symbol, company name, or any other criteria,
+                and you'll have instant access to relevant information.</p>
+            <br />
+            <p style={{ color: '#333', fontSize: '1.2rem', lineHeight: '1.5' }}>So, what are you waiting for? Give our trading aid a try and see for yourself how it can help take your trading to the next level.
+                We're confident you'll love it!</p>
         </div>
     );
 };
