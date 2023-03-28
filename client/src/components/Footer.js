4a8f4abdc018e677e5ee6c47a6d75adf7d1b06c1
MoniqueOg <moniqueogunk@icloud.com>
2023-03-28 22:04:05 +0100

    stringify json package

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,44 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Footer() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js';
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      symbols: [
+        {
+          proName: 'FOREXCOM:SPXUSD',
+          title: 'S&P 500',
+        },
+        {
+          proName: 'FOREXCOM:NSXUSD',
+          title: 'US 100',
+        },
+        {
+          proName: 'FX_IDC:EURUSD',
+          title: 'EUR/USD',
+        },
+        {
+          proName: 'BITSTAMP:BTCUSD',
+          title: 'Bitcoin',
+        },
+        {
+          proName: 'BITSTAMP:ETHUSD',
+          title: 'Ethereum',
+        },
+      ],
+      showSymbolLogo: true,
+      colorTheme: 'dark',
+      isTransparent: false,
+      displayMode: 'adaptive',
+      locale: 'en',
+    });
+    const parent = document.getElementsByClassName('tradingview-widget-container status-bar')[0];
+    parent.appendChild(script);
+    return () => {
+      parent.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="tradingview-widget-container status-bar">
-        <div className="tradingview-widget-container__widget"></div>
-        <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/" rel="noopener noreferrer" target="_blank"><span className="blue-text">Markets today</span></a> by TradingAid</div>
-        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-          {`{
-      "symbols": [
-        {
-          "proName": "FOREXCOM:SPXUSD",
-          "title": "S&P 500"
-        },
-        {
-          "proName": "FOREXCOM:NSXUSD",
-          "title": "US 100"
-        },
-        {
-          "proName": "FX_IDC:EURUSD",
-          "title": "EUR/USD"
-        },
-        {
-          "proName": "BITSTAMP:BTCUSD",
-          "title": "Bitcoin"
-        },
-        {
-          "proName": "BITSTAMP:ETHUSD",
-          "title": "Ethereum"
-        }
-      ],
-      "showSymbolLogo": true,
-      "colorTheme": "dark",
-      "isTransparent": false,
-      "displayMode": "adaptive",
-      "locale": "en"
-    }`}
-        </script>
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a href="https://www.tradingview.com/markets/" rel="noopener noreferrer" target="_blank">
+          <span className="blue-text">Markets today</span>
+        </a> by TradingAid
       </div>
-  );
+    </div>
+  ); 
 }
+
 
 export default Footer;
 
--- client/src/components/Stocks.js
+++ client/src/components/Stocks.js
@@ -1,33 +1,48 @@
-import { React, useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import stockData from '../data.json';
 
-
 function Stocks() {
-    const [searchParams] = useSearchParams();
+    const location = useLocation();
     const [results, setResults] = useState([]);
 
     useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
         const term = searchParams.get('search');
         if (term != null) {
-          console.log(term);
-          const filteredData = stockData.filter((item) => item.company.toLowerCase().includes(term.toLowerCase()));
-          setResults(filteredData);
-          console.log(filteredData);
+            const filteredData = stockData.filter((item) =>
+                item.company.toLowerCase().includes(term.toLowerCase())
+            );          
+            //collect symbols from filtered data utilizing a map ending up with an array of strings
+            //make a request to the market stack api(retrieving data from api)
+            //merge live data with filtered data
+            const symbols = filteredData.map((item) => item.symbol);
+            const symbolString = symbols.join(','); console.log(symbolString);
+            const url = `http://api.marketstack.com/v1/eod?access_key=d92d6bf719499bc80eff87d14dd277f8&symbols=${symbolString}`;
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    const updatedData = filteredData.map((item) => {
+                        const latestPrice = data.data.find((stock) => stock.symbol === item.symbol).close;
+                        return { ...item, latestPrice };
+                    });
+                    setResults(updatedData);
+                });
         } else {
-          setResults(stockData);
+            setResults(stockData);     
         }
-      }, [searchParams]);
-
+    }, [location.search]);
 
     return (
         <div>
             <h1>Search Results</h1>
-            <ul>
+            <ul style={{ padding: '50px' }}>
                 {results.map((item) => (
                     <li key={item.symbol}>
-                        <h2>{item.company}</h2>
+                        <h2>{item.company}   |   {item.symbol} </h2>
+                        <p>Latest Price: {item.latestPrice}</p>
                         <p>{item.description}</p>
+                        <br />
                     </li>
                 ))}
             </ul>
